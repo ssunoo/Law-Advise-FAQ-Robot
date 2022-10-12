d7f6a27cb08d06e2b0e9397bddaf360fc2bfe12b
ssunoo <ovo.snow.ovo@gmail.com>
2022-10-12 14:39:51 +0800

    feat:change input mode

--- src/faq.py
+++ src/faq.py
@@ -1,6 +1,7 @@
 import json
 import jieba
 import jieba.posseg as pseg
+import sys
 
 chineseDict = "../dict.txt.big"
 dataset = "../dataset/output.json"
@@ -24,32 +25,34 @@ with open(preprocessingData, 'r', encoding="utf-8") as data:
         questionsVec.append(vec)
         jsonObj = data.readline()
 
-query = input("Enter your question: ")
-queryWordList = pseg.lcut(query)
-queryVec = {}
-for word, attribute in queryWordList:
-    queryVec[word] = queryVec.get(word, 0) + 1
-disQuery = 0
-for w in queryVec.values():
-    disQuery += w**2
-disQuery = disQuery**(0.5)
+# query = input("Enter your question: ")
+print("Enter your question: ")
+for query in sys.stdin:
+    queryWordList = pseg.lcut(query)
+    queryVec = {}
+    for word, attribute in queryWordList:
+        queryVec[word] = queryVec.get(word, 0) + 1
+    disQuery = 0
+    for w in queryVec.values():
+        disQuery += w**2
+    disQuery = disQuery**(0.5)
 
-similarQuestionDict = {}
+    similarQuestionDict = {}
 
-for ids, question in enumerate(questionsVec):
-    product = 0
-    for word in queryVec:
-        product += queryVec[word] * question.get(word, 0)
-    
-    disQuestion = 0
-    for w in question.values():
-        disQuestion += w**2
-    disQuestion = disQuestion**(0.5)
+    for ids, question in enumerate(questionsVec):
+        product = 0
+        for word in queryVec:
+            product += queryVec[word] * question.get(word, 0)
 
-    similarQuestionDict[ids] = product / (disQuery * disQuestion)
+        disQuestion = 0
+        for w in question.values():
+            disQuestion += w**2
+        disQuestion = disQuestion**(0.5)
 
-sortSimilarQuestionDict = sorted(similarQuestionDict.items(), key=lambda x: x[1], reverse=True)
-print(questionsList[sortSimilarQuestionDict[0][0]]["a:"])
+        similarQuestionDict[ids] = product / (disQuery * disQuestion)
+
+    sortSimilarQuestionDict = sorted(similarQuestionDict.items(), key=lambda x: x[1], reverse=True)
+    print(questionsList[sortSimilarQuestionDict[0][0]]["a:"])
 
 
 
